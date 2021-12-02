7ed88e9a8e6e4280787a90f5835b432b77e36e9f
bashbunni <bashbunni.io@gmail.com>
2021-12-01 21:29:59 -0800

    feat: one gold star closer to a breakdown

--- day1/three_measurement.go
+++ day1/three_measurement.go
@@ -1,39 +1,50 @@
 package day1
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func ThreeMeasurement() {
+func ThreeMeasurement() int {
 	// A: [1, 2, 3]
 	// go through data line by line, then add the value on that line to the letter's value
-	f, err := os.Open("./day1/sample_data2.txt")
+	data, err := os.ReadFile("./day1/data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	scanner := bufio.NewScanner(f)
-	values := make(map[string]int)
-	for scanner.Scan() {
-		val := strings.Fields(scanner.Text())
-		var currValue int
-		for i := 0; i < len(val); i++ {
-			num, _ := strconv.Atoi(val[i])
-			if num == 0 {
-				// it's a letter
-				if _, ok := values[val[i]]; ok {
-					values[val[i]] += currValue
-				} else {
-					values[val[i]] = currValue
-				}
-			} else {
-				currValue = num
-			}
+	lines := strings.Split(string(data), "\n")
+	nums := stringToInt(lines)
+	// grab all the numbers from the file
+	// get the sum of three sequential numbers
+	// get the sum of i+1 three sequential numbers
+	// check if sum1 < sum2 ? increase++
+	increased := 0
+	for i := 0; i < len(nums)-3; i++ {
+		sum1 := nums[i] + nums[i+1] + nums[i+2]
+		sum2 := nums[i+1] + nums[i+2] + nums[i+3]
+		if sum1 < sum2 {
+			increased++
 		}
 	}
-	fmt.Print("A" < "B")
+	return increased
+}
+
+func stringToInt(strings []string) []int {
+	var nums []int
+	for _, value := range strings {
+		num, _ := strconv.Atoi(value)
+		nums = append(nums, num)
+	}
+	return nums
+}
+
+func contains(nums []int, target int) bool {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	fmt.Printf("result from example given: %d\n", day1.CountDepthIncreasedFromFile("./day1/sample_data.txt"))
 	fmt.Printf("result from my puzzle data: %d\n", day1.CountDepthIncreasedFromFile("./day1/data.txt"))
-	day1.ThreeMeasurement()
+	fmt.Printf("%d\n", day1.ThreeMeasurement())
 }
