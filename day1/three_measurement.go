597e7e18eb5aaeb97c8c8022b580878ba420377d
bashbunni <bashbunni.io@gmail.com>
2021-12-01 19:16:21 -0800

    feat: get sum of measurements as a map

--- day1/three_measurement.go
+++ day1/three_measurement.go
@@ -1,11 +1,39 @@
 package day1
 
-import "adventofcode/utils"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func ThreeMeasurement() {
 	// A: [1, 2, 3]
 	// go through data line by line, then add the value on that line to the letter's value
-	utils.ReadData("./sample_data2.txt")
-
-
+	f, err := os.Open("./day1/sample_data2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	values := make(map[string]int)
+	for scanner.Scan() {
+		val := strings.Fields(scanner.Text())
+		var currValue int
+		for i := 0; i < len(val); i++ {
+			num, _ := strconv.Atoi(val[i])
+			if num == 0 {
+				// it's a letter
+				if _, ok := values[val[i]]; ok {
+					values[val[i]] += currValue
+				} else {
+					values[val[i]] = currValue
+				}
+			} else {
+				currValue = num
+			}
+		}
+	}
+	fmt.Print("A" < "B")
 }
